c6baecceac23c2fc18f5b568c30bc1b5391d10c7
AnastasiiaDmytruk <153641542+AnastasiiaDmytruk@users.noreply.github.com>
2025-05-19 15:14:48 +0200

    Featured Destinations Section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Hero from "../components/Hero.jsx";
+import FeaturedDestinations from "../components/FeaturedDestinations.jsx";
 
 const Home = () => {
   return (
     <>
       <Hero />
+      <FeaturedDestinations />
     </>
   );
 };
