9755ed6767b85a882ac324c50ca298262fac30f9
AnastasiiaDmytruk <153641542+AnastasiiaDmytruk@users.noreply.github.com>
2025-06-16 13:14:30 +0200

    Dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,11 @@ import Footer from "./components/Footer.jsx";
 import AllRooms from "./components/AllRooms.jsx";
 import RoomDetails from "./pages/RoomDetail.jsx";
 import MyBookings from "./pages/MyBookings.jsx";
+import HotelReg from "./components/HotelReg.jsx";
+import Layout from "./pages/hotelOwner/Layout.jsx";
+import Dashboard from "./pages/hotelOwner/Dashboard.jsx";
+import AddRoom from "./pages/hotelOwner/AddRoom.jsx";
+import ListRoom from "./pages/hotelOwner/ListRoom.jsx";
 
 const App = () => {
   const isOwnerPath = useLocation().pathname.includes("owner");
@@ -13,12 +18,18 @@ const App = () => {
   return (
     <div>
       {!isOwnerPath && <Navbar />}
+      {false && <HotelReg />}
       <div className="min-h-[70vh]">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/rooms" element={<AllRooms />} />
           <Route path="/rooms/:id" element={<RoomDetails />} />
           <Route path="/my-bookings" element={<MyBookings />} />
+          <Route path="/owner" element={<Layout />}>
+            <Route index element={<Dashboard />} />
+            <Route path="add-room" element={<AddRoom />} />
+            <Route path="list-room" element={<ListRoom />} />
+          </Route>
         </Routes>
       </div>
       <Footer />
