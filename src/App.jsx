a2c2507564fa26c280c78d9c2a6aa0912732c898
AnastasiiaDmytruk <153641542+AnastasiiaDmytruk@users.noreply.github.com>
2025-06-16 11:12:22 +0200

    My Bookings Page

--- src/App.jsx
+++ src/App.jsx
@@ -13,12 +13,12 @@ const App = () => {
   return (
     <div>
       {!isOwnerPath && <Navbar />}
-      <div className="min-h-[70hv]">
+      <div className="min-h-[70vh]">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/rooms" element={<AllRooms />} />
           <Route path="/rooms/:id" element={<RoomDetails />} />
-          <Route path="/rooms/my-bookings" element={<MyBookings />} />
+          <Route path="/my-bookings" element={<MyBookings />} />
         </Routes>
       </div>
       <Footer />
